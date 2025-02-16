37e80e49e750f0c1de1fe510fb30dd1317fdacee
nguyentk <nguyentk@petarainsoft.com>
2025-02-16 12:55:04 +0700

    Add login button

--- src/components/molecules/Sidebar/Sidebar.tsx
+++ src/components/molecules/Sidebar/Sidebar.tsx
@@ -1,13 +1,14 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import styled from '@emotion/styled';
 
 import Grid, { Grid2Props } from '@mui/material/Grid2';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
-
+import LogoutIcon from '@mui/icons-material/Logout';
 import { MAIN_PATH } from 'routes';
 
 import BookOnlineIcon from '@mui/icons-material/BookOnline';
+import Typography from 'components/atoms/Typography';
 // import LocalHospitalIcon from '@mui/icons-material/LocalHospital';
 
 type Props = Grid2Props;
@@ -29,9 +30,16 @@ const sidebars = [
 ];
 
 const Sidebar = ({ className }: Props) => {
+  const navigate = useNavigate();
+  const logout = () => {
+    localStorage.clear();
+
+    navigate('/');
+  };
+
   return (
-    <Grid p={2} height="100%" size="auto" className={className}>
-      <List>
+    <Grid p={2} height="100%" size="auto" className={className} display="flex" flexDirection="column">
+      <List sx={{ display: 'flex', flexDirection: 'column', rowGap: 2, flex: 1 }}>
         {sidebars.map(item => (
           <ListItem key={item.label} sx={{ display: 'flex', columnGap: 1 }}>
             {item.icon && <item.icon />}
@@ -39,6 +47,11 @@ const Sidebar = ({ className }: Props) => {
           </ListItem>
         ))}
       </List>
+
+      <Grid display="flex" columnGap={1} width="100%" sx={{ cursor: 'pointer' }} onClick={logout}>
+        <LogoutIcon />
+        <Typography>로그아웃</Typography>
+      </Grid>
     </Grid>
   );
 };
